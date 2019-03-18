60c790124fc358ed23ac2f21c9bb978e6d1dd89b
MateusSilveira98 <mateus.costa98.dev@gmail.com>
2019-03-18 10:04:50 -0300

    First Commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express = require('express');
+
+const app: express.Application = express();
+
+app.get('/', (req, res) => {
+    res.send('Hellow World!');
+});
+
+app.listen(3000, () => {
+    console.log('listening on http://localhost:3000!');
+});

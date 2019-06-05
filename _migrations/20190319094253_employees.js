15ae5cfab64b22719004249b5906940a2badeaa5
Mateus Costa <mateus.costa98.dev@gmail.com>
2019-06-04 22:58:52 -0300

    Add email field

--- _migrations/20190319094253_employees.js
+++ _migrations/20190319094253_employees.js
@@ -4,7 +4,8 @@ exports.up = (knex, Promise) => {
   const schema = knex.schema.createTable('employees', (table) => {
     table.increments('id').primary();
     table.string('name');
-    table.decimal('salary', 10, 2);
+    table.string('email');
+    table.decimal('salary', 10, 2).defaultTo(0);
     table.string('image').defaultTo('https://res.cloudinary.com/mateus-costa/image/upload/v1556203484/wtt/sem-foto.jpg');
     table.string('modality_of_contracting');
     table.string('profession');
